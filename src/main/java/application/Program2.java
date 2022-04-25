63fc2af55e1dec62ca2c9ee2c08f8b8a77195145
HadryanSilva <hadryan2001@gmail.com>
2022-04-25 08:43:39 -0300

    DepartmentDao implementation and general fixes

--- src/main/java/application/Program2.java
+++ src/main/java/application/Program2.java
@@ -2,6 +2,7 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
+import model.entities.Department;
 
 import java.util.Scanner;
 
@@ -11,16 +12,25 @@ public class Program2 {
 
         Scanner read = new Scanner(System.in);
 
+        System.out.println("------- TEST 1: findById -------");
+        DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+        Department department = departmentDao.findById(3);
+        System.out.println(department);
+        System.out.println();
+
+        /*
         System.out.println("------- TEST 1: delete -------");
-        DepartmentDao department = DaoFactory.createDepartmentDao();
+        DepartmentDao department1 = DaoFactory.createDepartmentDao();
         System.out.print("Enter the id to delete: ");
         int id = read.nextInt();
-        department.deleteById(id);
+        department1.deleteById(id);
         System.out.println("Delete complete");
         System.out.println();
 
-
-
+        System.out.println("------- TEST 2: update -------");
+        DepartmentDao newDepartment = DaoFactory.createDepartmentDao();
+        newDepartment.findById(3);
+        newDepartment.update();*/
 
 
     }
